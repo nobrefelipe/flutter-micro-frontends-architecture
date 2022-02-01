41b4d8d1df1ad350106a473c6f3ead3d52016947
Felipe Nobre <felipe.nobre@shift.online>
2022-02-01 18:51:45 +0000

    remove material

--- micro_apps/search/lib/app/presenter/search_button.dart
+++ micro_apps/search/lib/app/presenter/search_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:micro_core/micro_core.dart';
 import 'package:search/app/presenter/search_results.dart';
 
